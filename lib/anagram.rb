d3843f91223a4c356571c2934202f20f8473a1c6
Will Swanson <wcs97214@gmail.com>
2018-06-29 08:47:37 -0700

    add first test pass

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,19 @@
 class Ragaman
 
   def initialize(source,candidate)
-    @original_input = souce.downcase
+    @original_input = source.downcase
     @input_to_analyze = candidate.downcase
   end
 
   def ragamanner()
+    result_message = ""
+    @original_single_word = @original_input.split("")
+    @input_single_word = @input_to_analyze.split("")
+    if @original_single_word.sort.join("") == @input_single_word.sort.join("")
+      result_message = "is anagram"
+    else
+      result_message = "is not anagram"
+    end
   end
 
 end
