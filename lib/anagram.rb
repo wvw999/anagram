3790fb35c7a0ca9585320bdebbb975e570625146
Will Swanson <wcs97214@gmail.com>
2018-06-29 08:56:33 -0700

    add failing test 2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,9 +11,21 @@ class Ragaman
     @input_single_word = @input_to_analyze.split("")
     if @original_single_word.sort.join("") == @input_single_word.sort.join("")
       result_message = "is anagram"
+    elsif @original_single_word.length == @input_single_word.length
+      mantarigger()
     else
       result_message = "is not anagram"
     end
+  end
+
+  def mantarigger()
+    result_message = ""
+    not_antigram = 0
+    # if 
+    #   result_message = "is antigram"
+    # else
+    #   result_message = "is not anagram"
+    # end
   end
 
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -6,4 +6,8 @@ describe('#anagram') do
     is_anagram = Ragaman.new("frogs", "sgorf")
     expect(is_anagram.ragamanner()).to(eq("is anagram"))
   end
+  it('checks if two words are antigrams') do
+    is_anagram = Ragaman.new("bubble", "grunt")
+    expect(is_anagram.ragamanner()).to(eq("is antigram"))
+  end
 end
