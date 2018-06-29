19e84621c7dfa14ad6acb6a032ff034d369f2441
Will Swanson <wcs97214@gmail.com>
2018-06-29 12:04:06 -0700

    wrestling with logic on anagram antigram for phrases

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -40,47 +40,56 @@ class Ragaman
       false
     end
   end
+
   # phrase anagrammer
   def hangarapearms()
-    part_of_phrase_is_anagram = 1
-    part_of_phrase_is_antigram = 1
+    part_of_phrase_is_anagram = 0
+    part_of_phrase_is_antigram = 0
 
     # loop to check bits of phrase goes here
     @input_multiple_word = @input_to_analyze.split(" ")
     length_of_array = @input_multiple_word.length
     @parent_combination_array = []
-    (1..length_of_array).times do |combination_step|
+    (1..length_of_array).each do |combination_step|
       @all_combinations_of_words = @input_multiple_word.combination(combination_step).to_a
       @all_combinations_of_words.each do |push_to_parent|
         @parent_combination_array.push(push_to_parent)
       end
     end
 
-      # search through @all_combinations_of_words for anagram
+    # search through @all_combinations_of_words for anagram
     @parent_combination_array.each do |compare_anagram|
-      if compare_anagram.join("").to_a.sort.join("") = @original_single_word.sort.join("")
-        @result_message += "#{compare_anagram} is anagram of #{@original_input}\n"
-        part_of_phrase_is_anagram = 0
+      print @parent_combination_array
+      puts "fired anagramm checker for #{compare_anagram}"
+      value1 = compare_anagram.join("").split("").sort.join("")
+      value2 = @original_single_word.sort.join("")
+      if compare_anagram.join("").split("").sort.join("") == @original_single_word.sort.join("")
+        puts "here is joined: #{value1} \n and here is original join - #{value2}"
+        @result_message += "#{compare_anagram.join(" ")} is anagram of #{@original_input}\n"
+        part_of_phrase_is_anagram = 1
       end
     end
 
     # search through @all_combinations_of_words for antigram
     @parent_combination_array.each do |compare_antigram|
-      compare_antigram.join("").to_a.each do |antigram_check_level_one|
+      puts "fired anagramm checker for #{compare_antigram}"
+      compare_antigram.join("").split("").each do |antigram_check_level_one|
+        puts "here is antigram compare #{compare_antigram.join("").split("").each}"
         @original_single_word.each do |antigram_check_level_two|
+          puts "and here is antigram_check_level_two #{antigram_check_level_two}"
           if antigram_check_level_one == antigram_check_level_two
             part_of_phrase_is_antigram = 0
           end
         end
       end
-      if part_of_phrase_is_antigram = 1
-        @result_message += "#{compare_antigram} is antigram of #{@original_input}\n"
+      if part_of_phrase_is_antigram == 1
+        @result_message += "#{compare_antigram.join(" ")} is antigram of #{@original_input}\n"
       end
     end
 
     # send back result if neither anagram nor antigram is found
     if part_of_phrase_is_anagram == 0 && part_of_phrase_is_antigram == 0
-      @result_message += "#{compare_antigram} is not an anagram or antigram of #{@original_input}\nand no combinations of #{compare_antigram} was found to be an anagram or antigram, either"
+      @result_message += "#{@input_to_analyze} is not an anagram or antigram of #{@original_input}\nand no combinations of #{@input_to_analyze} was found to be an anagram or antigram, either"
     end
   end
 
