05334be99f6c613d49a572e3a24639aced8a2ded
Will Swanson <wcs97214@gmail.com>
2018-06-29 11:41:12 -0700

    still working on things

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -23,6 +23,7 @@ class Ragaman
       result_message = "#{@input_to_analyze} is not anagram or antigram of #{@original_input}"
     end
   end
+
   # antigrammer
   def mantarigger()
     is_antigram = 1
@@ -41,23 +42,30 @@ class Ragaman
   end
   # phrase anagrammer
   def hangarapearms()
-    part_of_phrase_is_anagram = 0
+    part_of_phrase_is_anagram = 1
     part_of_phrase_is_antigram = 1
 
     # loop to check bits of phrase goes here
     @input_multiple_word = @input_to_analyze.split(" ")
     length_of_array = @input_multiple_word.length
-    @all_combinations_of_words = (1..length_of_array).flat_map{|size| @input_multiple_word(size).to_a}
-      # compare_anagram each for anagram
-    @all_combinations_of_words.each do |compare_anagram|
-      if compare_anagram.join("").to_a.sort.join("") = @original_single_word.sort.join("")
-        @result_message += "#{compare_anagram} is anagram of #{@original_input}\n"
-        part_of_phrase_is_anagram = 1
+    @parent_combination_array = []
+    (1..length_of_array).times do |combination_step|
+      @all_combinations_of_words = @input_multiple_word.combination(combination_step).to_a
+      @all_combinations_of_words.each do |push_to_parent|
+        @parent_combination_array.push(push_to_parent)
       end
     end
 
-    # compare_antigram each for antigram
-    @all_combinations_of_words.each do |compare_antigram|
+      # search through @all_combinations_of_words for anagram
+    @parent_combination_array.each do |compare_anagram|
+      if compare_anagram.join("").to_a.sort.join("") = @original_single_word.sort.join("")
+        @result_message += "#{compare_anagram} is anagram of #{@original_input}\n"
+        part_of_phrase_is_anagram = 0
+      end
+    end
+
+    # search through @all_combinations_of_words for antigram
+    @parent_combination_array.each do |compare_antigram|
       compare_antigram.join("").to_a.each do |antigram_check_level_one|
         @original_single_word.each do |antigram_check_level_two|
           if antigram_check_level_one == antigram_check_level_two
@@ -70,13 +78,10 @@ class Ragaman
       end
     end
 
+    # send back result if neither anagram nor antigram is found
+    if part_of_phrase_is_anagram == 0 && part_of_phrase_is_antigram == 0
+      @result_message += "#{compare_antigram} is not an anagram or antigram of #{@original_input}\nand no combinations of #{compare_antigram} was found to be an anagram or antigram, either"
     end
-    # will need to build two-stage check, whole phrase and combinations of parts of phrase
-    # if part_of_phrase_is_anagram == 1
-    #   true
-    # else
-    #   false
-    # end
   end
 
 end
